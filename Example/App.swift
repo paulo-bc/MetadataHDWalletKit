72acc9d346c7346410374d5c5b3657861998357b
paulo-bc <60097845+paulo-bc@users.noreply.github.com>
2023-10-24 16:40:04 +0100

    Xcode 15 support (#1)
    [fix] add @_optimize(none) to a Data extension in Data+Script

---
+++ Example/App.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+import MetadataHDWalletKit
+
+@main
+struct Example: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
+}
--- Package.swift
+++ Package.swift
@@ -1,42 +1,40 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
     name: "MetadataHDWalletKit",
     platforms: [
-        .iOS(.v14), .macOS(.v11)
+        .iOS(.v15),
+        .macOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MetadataHDWalletKit",
-            targets: ["MetadataHDWalletKit"]),
+            targets: ["MetadataHDWalletKit"]
+        )
     ],
     dependencies: [
         .package(
-            name: "secp256k1",
             url: "https://github.com/Boilertalk/secp256k1.swift.git",
-            from: "0.1.0"
+            from: "0.1.7"
         ),
         .package(
-            name: "CryptoSwift",
             url: "https://github.com/krzyzanowskim/CryptoSwift.git",
-            from: "1.4.2"
-        ),
+            from: "1.8.0"
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MetadataHDWalletKit",
             dependencies: [
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
-                .product(name: "secp256k1", package: "secp256k1")
-            ]),
+                .product(name: "secp256k1", package: "secp256k1.swift")
+            ]
+        ),
         .testTarget(
             name: "MetadataHDWalletKitTests",
-            dependencies: ["MetadataHDWalletKit"]),
+            dependencies: ["MetadataHDWalletKit"]
+        )
     ]
 )
